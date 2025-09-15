49ab69e55152b4255a452421c180cc08056fc55a
dirrialsss <dirrialss@gmail.com>
2025-09-15 11:54:45 +0300

    execution of the task

--- pract_math_task/math_task.c
+++ pract_math_task/math_task.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    double alpha_deg, alpha_rad, z1, z2;
+
+    printf("Enter alpha in degrees:  ");
+    scanf("%lf", &alpha_deg);
+
+    alpha_rad = alpha_deg * M_PI / 180.0;
+
+    z1 = (sin(2*alpha_rad)+sin(5*alpha_rad)-sin(3*alpha_rad))/(cos(alpha_rad)-cos(3*alpha_rad)+cos(5*alpha_rad));
+
+    z2 = tan(3 * alpha_rad);
+
+    printf("Our results:\n");
+    printf(" z1 = %.4f\n",z1);
+    printf(" z2 = %.4f\n",z2);
+
     return 0;
 }
